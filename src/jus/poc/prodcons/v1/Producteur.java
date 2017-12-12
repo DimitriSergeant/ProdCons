fd4389e7b9dca2d6337dfbdc3a808707ea2ec2c4
Dimitri <xdusster@gmail.com>
2017-12-12 10:57:04 +0100

    Arborescence : Ajout de la classe Properties.java dans options

--- src/jus/poc/prodcons/v1/Producteur.java
+++ src/jus/poc/prodcons/v1/Producteur.java
@@ -7,8 +7,7 @@ import jus.poc.prodcons._Producteur;
 
 public class Producteur extends Acteur implements _Producteur {
 
-	protected Producteur(int type, Observateur observateur, int moyenneTempsDeTraitement,
-			int deviationTempsDeTraitement) throws ControlException {
+	protected Producteur(int type, Observateur observateur, int moyenneTempsDeTraitement, int deviationTempsDeTraitement) throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
 		// TODO Auto-generated constructor stub
 	}
