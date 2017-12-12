00704f8d41d6abe4acc337a00a9b408c1b83d37f
Dimitri <xdusster@gmail.com>
2017-12-12 15:41:45 +0100

    Commit pour créer la branche

--- src/jus/poc/prodcons/v1/Producteur.java
+++ src/jus/poc/prodcons/v1/Producteur.java
@@ -7,29 +7,15 @@ import jus.poc.prodcons._Producteur;
 
 public class Producteur extends Acteur implements _Producteur {
 
-    protected Producteur(int type, Observateur observateur, int moyenneTempsDeTraitement,
-	    int deviationTempsDeTraitement) throws ControlException {
-	super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
-    }
+	protected Producteur(int type, Observateur observateur, int moyenneTempsDeTraitement, int deviationTempsDeTraitement) throws ControlException {
+		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		// TODO Auto-generated constructor stub
+	}
 
-    @Override
-    /*
-     * (non-Javadoc)
-     * 
-     * @see jus.poc.prodcons.Acteur#nombreDeMessages()
-     */
-    public int nombreDeMessages() {
-	return this.nombreDeMessages();
-    }
-
-    @Override
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Thread#run()
-     */
-    public void run() {
-
-    }
+	@Override
+	public int nombreDeMessages() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
