0e695d7f7e771259967c67eb4618f94e23f5a96f
Jordan JEAN <jordan73@live.fr>
2017-12-12 13:22:08 +0100

    lecture XML

--- src/jus/poc/prodcons/v1/Consommateur.java
+++ src/jus/poc/prodcons/v1/Consommateur.java
@@ -7,16 +7,29 @@ import jus.poc.prodcons._Consommateur;
 
 public class Consommateur extends Acteur implements _Consommateur {
 
-	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
-			int deviationTempsDeTraitement) throws ControlException {
-		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
-		// TODO Auto-generated constructor stub
-	}
+    protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
+	    int deviationTempsDeTraitement) throws ControlException {
+	super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+    }
 
-	@Override
-	public int nombreDeMessages() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jus.poc.prodcons.Acteur#nombreDeMessages()
+     */
+    public int nombreDeMessages() {
+	return this.nombreDeMessages();
+    }
+
+    @Override
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Thread#run()
+     */
+    public void run() {
+
+    }
 
 }
--- src/jus/poc/prodcons/v1/TestProdCons.java
+++ src/jus/poc/prodcons/v1/TestProdCons.java
@@ -1,32 +1,61 @@
 package jus.poc.prodcons.v1;
 
+import java.util.Map;
+import java.util.Properties;
+
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons.Simulateur;
 
 public class TestProdCons extends Simulateur {
-	private static String configurationFile = "conf.v3.xml";
-	
-	private int nbProd = 0;
-	private int nbCons = 0;
-	private int nbBuffer = 0;
-	private int tempsMoyenProduction = 0;
-	private int deviationtempsMoyenProduction = 0;
-	private int tempsMoyenConsommation = 0;
-	private int deviationtempsMoyenConsommation = 0;
-	private int nombreMoyenProduction = 0;
-	private int deviationNombreMoyenProduction = 0;
-	
-	private int signalNombreDeProduction = 0;
-	
-	private ProdCons buffer;
-	
-	public TestProdCons(Observateur observateur){super(observateur);}
+    private static String configurationFile = "options.xml";
 
-	@Override
-	protected void run() throws Exception {
-		// TODO Auto-generated method stub
-		}
-	public static void main(String[] args){new TestProdCons(new Observateur()).start();}
-	
+    private int nbProd = 0;
+    private int nbCons = 0;
+    private int nbBuffer = 0;
+    private int tempsMoyenProduction = 0;
+    private int deviationTempsMoyenProduction = 0;
+    private int tempsMoyenConsommation = 0;
+    private int deviationTempsMoyenConsommation = 0;
+    private int nombreMoyenDeProduction = 0;
+    private int deviationNombreMoyenDeProduction = 0;
+    private int nombreMoyenNbExemplaire = 0;
+    private int deviationNombreMoyenNbExemplaire = 0;	
+
+    private int signalNombreDeProduction = 0;
+
+    private ProdCons buffer;
+
+    public TestProdCons(Observateur observateur) {
+	super(observateur);
+    }
+
+    @Override
+    /*
+     * (non-Javadoc)
+     * @see jus.poc.prodcons.Simulateur#run()
+     */
+    protected void run() throws Exception {
+	Properties properties = new Properties();
+	properties.loadFromXML(ClassLoader.getSystemResourceAsStream("jus/poc/prodcons/options/" + configurationFile));
+	String key;
+	int value;
+	Class<?> thisOne = getClass();
+	for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+	    key = (String) entry.getKey();
+	    value = Integer.parseInt((String) entry.getValue());
+	    thisOne.getDeclaredField(key).set(this, value);
+	}
+	if(false){
+	    for(Map.Entry<Object, Object> entry : properties.entrySet()){
+		key = (String) entry.getKey();
+		System.out.println(key + "=" + thisOne.getDeclaredField(key).get(this));
+	    }
+	}
+	observateur.init(nbProd, nbCons, nbBuffer);
+    }
+
+    public static void main(String[] args) {
+	new TestProdCons(new Observateur()).start();
+    }
+
 }
-
