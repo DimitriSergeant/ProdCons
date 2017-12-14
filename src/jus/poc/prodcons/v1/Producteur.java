96920359063641500d4e4cb565080cda4ac66b47
Jordan JEAN <jordan73@live.fr>
2017-12-14 15:35:31 +0100

    restore

--- src/jus/poc/prodcons/v1/Producteur.java
+++ src/jus/poc/prodcons/v1/Producteur.java
@@ -7,21 +7,20 @@ import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons._Producteur;
 
 public class Producteur extends Acteur implements _Producteur {
-	final private boolean DEBUG = true;
-	private int nbMessageATraiter;
-	private Aleatoire VAtemps;
-	private Aleatoire VAproduction;
-	private ProdCons tampon;
+    final private boolean DEBUG = true;
+    private int nbMessageATraiter;
+    private Aleatoire VAtemps;
+    private Aleatoire VAproduction;
+    private ProdCons tampon;
 
-	protected Producteur(Observateur observateur, ProdCons tampon, int moyenneTempsDeTraitement,
-			int deviationTempsDeTraitement, int nombreMoyenDeProduction, int deviationNombreMoyenDeProduction)
-			throws ControlException {
-		super(typeProducteur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+    protected Producteur(Observateur observateur, ProdCons tampon, int moyenneTempsDeTraitement,
+	    int deviationTempsDeTraitement, int nombreMoyenDeProduction, int deviationNombreMoyenDeProduction)
+	    throws ControlException {
+	super(typeProducteur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
 
-		VAproduction = new Aleatoire(nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
-		VAtemps = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+	VAproduction = new Aleatoire(nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
+	VAtemps = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
 
-<<<<<<< HEAD
 	VAproduction = new Aleatoire(nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
 	VAtemps = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
 
@@ -73,56 +72,6 @@ public class Producteur extends Acteur implements _Producteur {
 	    }
 
 	    this.nbMessageATraiter--;
-=======
-		this.nbMessageATraiter = VAproduction.next();
-		this.tampon = tampon;
->>>>>>> branch 'Jordan' of https://github.com/DimitriSergeant/ProdCons.git
 	}
-
-	@Override
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see jus.poc.prodcons.Acteur#nombreDeMessages()
-	 */
-	public int nombreDeMessages() {
-		return this.nbMessageATraiter;
-	}
-
-	@Override
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Thread#run()
-	 */
-	public void run() {
-
-		MessageX m;
-
-		while (nbMessageATraiter > 0) {
-			m = new MessageX(this.identification(), "Content");
-
-			try {
-				Thread.sleep(VAtemps.next());
-			} catch (InterruptedException e) {
-				System.out.println(e.toString());
-				e.printStackTrace();
-			}
-
-			try {
-				this.tampon.put(this, m);
-				if (DEBUG)
-					System.out.println("Le message : " + m.toString() + " a été ajouté au tampon ");
-			} catch (InterruptedException e) {
-				System.out.println(e.toString());
-				e.printStackTrace();
-			} catch (Exception e) {
-				System.out.println(e.toString());
-				e.printStackTrace();
-			}
-
-			this.nbMessageATraiter--;
-		}
-	}
-
+    }
 }
